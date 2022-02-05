b2386786c8502aeaee43f2c6f85ebb35fb644a0d
cd1412 <chiduong1412@gmail.com>
2022-02-04 23:12:25 -0600

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(count)
+
+count = 10
+while count >= 1
+    puts "#{count} SECOND(S)!"
+    count -= 1
+end 
+"HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(count)
+
+    count = 10
+while count >= 1
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+end 
+"HAPPY NEW YEAR!"
+
 end
